87913666c1a510025fcd87757a2258acfbdf1e66
sile <sinisa.buncic7@gmail.com>
2018-07-06 11:40:50 +0200

    removed color and size filters in shop template

--- resources/views/pages/shop.blade.php
+++ resources/views/pages/shop.blade.php
@@ -103,34 +103,6 @@
                             </div>
                         </div>
 
-                        <div class="widget color mb-70">
-                            <h6 class="widget-title mb-30">Filter by Color</h6>
-                            <div class="widget-desc">
-                                <ul class="d-flex justify-content-between">
-                                    <li class="gray"><a href="#"><span>(3)</span></a></li>
-                                    <li class="red"><a href="#"><span>(25)</span></a></li>
-                                    <li class="yellow"><a href="#"><span>(112)</span></a></li>
-                                    <li class="green"><a href="#"><span>(72)</span></a></li>
-                                    <li class="teal"><a href="#"><span>(9)</span></a></li>
-                                    <li class="cyan"><a href="#"><span>(29)</span></a></li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        <div class="widget size mb-50">
-                            <h6 class="widget-title mb-30">Filter by Size</h6>
-                            <div class="widget-desc">
-                                <ul class="d-flex justify-content-between">
-                                    <li><a href="#">XS</a></li>
-                                    <li><a href="#">S</a></li>
-                                    <li><a href="#">M</a></li>
-                                    <li><a href="#">L</a></li>
-                                    <li><a href="#">XL</a></li>
-                                    <li><a href="#">XXL</a></li>
-                                </ul>
-                            </div>
-                        </div>
-
                         <div class="widget recommended">
                             <h6 class="widget-title mb-30">Recommended</h6>
 
