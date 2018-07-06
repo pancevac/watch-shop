443ab77eb56347664bce3d9cfa8b5f857c548f07
sile <sinisa.buncic7@gmail.com>
2018-07-06 12:17:50 +0200

    changed coupons code (hard coded)

--- resources/views/inc/discount.blade.php
+++ resources/views/inc/discount.blade.php
@@ -2,16 +2,16 @@
     <!-- Single Discount Area -->
     <div class="single-discount-area">
         <h5>Free Shipping &amp; Returns</h5>
-        <h6><a href="#">BUY NOW</a></h6>
+        <h6><a href="{{ route('shop.index') }}">BUY NOW</a></h6>
     </div>
     <!-- Single Discount Area -->
     <div class="single-discount-area">
-        <h5>20% Discount for all dresses</h5>
-        <h6>USE CODE: Colorlib</h6>
+        <h5>20% Discount for all watches</h5>
+        <h6>USE CODE: DEF456</h6>
     </div>
     <!-- Single Discount Area -->
     <div class="single-discount-area">
-        <h5>20% Discount for students</h5>
-        <h6>USE CODE: Colorlib</h6>
+        <h5>30% Discount for students</h5>
+        <h6>USE CODE: STUDENTARIJA</h6>
     </div>
 </section>
